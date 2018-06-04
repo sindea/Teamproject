8cebf8dcb738b46dffb624150ce416afd0fae2cf
Sindea <auatjd12@naver.com>
2018-06-04 17:31:59 +0900

    commit

--- web_movie/src/main/java/com/movie/controller/IndexController.java
+++ web_movie/src/main/java/com/movie/controller/IndexController.java
@@ -1,8 +1,10 @@
 package com.movie.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+@Controller
 public class IndexController {
 
 	
